d897e1090d96f7d9d40b18100fea2874f9301cf0
S. Prabhu <73574125+prabhu88@users.noreply.github.com>
2023-01-23 16:10:23 +0530

    Update example.php

--- example.php
+++ example.php
@@ -1,5 +1,20 @@
 <?php
-require_once('src/Logger.php');
-use Cpl\Logger
+require_once 'src/Logger.php';
+use Cpl\Logger;
 
+if (class_exists('Cpl\Logger')) {
+    $logger = new Logger();
+    $path = __DIR__.DIRECTORY_SEPARATOR.'log_folder'.DIRECTORY_SEPARATOR ;
+    $log_with_location = new Logger($path);
+}
+
+$logger->log('hello',"logger","info");
+$logger->log('hello',"sad","debug");
+$logger->log('ello',"sad","error");
+
+$log_with_location->log('hello',"test","info");
+$log_with_location->log('hello',"sad","debug");
+$log_with_location->log('ello',"sad","error");
+
+echo "Awesome Logger";
 ?>
